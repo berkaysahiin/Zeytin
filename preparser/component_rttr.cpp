3c448bc978ee3d4a34ad276b60f1c2fa2f8eb490
Berkay <berkaysahindev@gmail.com>
2026-01-06 22:16:00 +0300

    delete

--- preparser/component_rttr.cpp
+++
@@ -1,9 +0,0 @@
-#include "rttr/registration.h"
-
-import zeytin.component;
-
-RTTR_REGISTRATION
-{
-    rttr::registration::class_<Component>("Component")
-        .constructor<>()(rttr::policy::ctor::as_object);
-}
