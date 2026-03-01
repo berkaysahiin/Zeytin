53eec769c71dd4c1b4a993ec1a34e0079b6ca75e
Berkay <berkaysahindev@gmail.com>
2026-03-01 17:54:13 +0300

    Use unused tags

--- engine/include/preparser.h
+++ engine/include/preparser.h
@@ -1,8 +1,3 @@
 #pragma once
 
 #define PROPERTY(...) [[clang::annotate(#__VA_ARGS__)]]
-
-// used for taging virtual component
-struct VirtualComponentTag {};
-
-#define VIRTUAL_COMPONENT  private: [[no_unique_address]] VirtualComponentTag ___virtual_component_tag; public:
